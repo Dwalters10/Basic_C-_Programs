793bbd0a0ef5e6be36a0e759da2ab6e266141d44
Dwalters10 <154628764+Dwalters10@users.noreply.github.com>
2024-05-21 03:17:13 -0600

    Initial commit

---
+++ WelcometoAcmeAccounting.cs/WelcometoAcmeAccounting.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace WelcometoAcmeAccounting.cs
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems Remember, we're Accounting on you!");
+            Console.ReadLine();
+        }
+    }
+}
